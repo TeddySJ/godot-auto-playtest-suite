b18abf35f44306b833e70b088f84b90a39f795b3
Teddy Sjöström <teddy.tyler@gmail.com>
2025-09-09 11:05:37 +0200

    Logging: Created a custom logging class that extends the base logger class

---
+++ addons/auto_play_suite_editor/logging/auto_play_suite_customlogger_test.gd
@@ -0,0 +1,21 @@
+extends AutoPlaySuiteLogger
+class_name AutoPlaySuiteCustomLogger_Test
+
+var time_passed : float = 0
+
+func setup():
+	forward_output_to_editor = true
+	write_to_output("Created a Test Logger!")
+
+func _process(delta: float) -> void:
+	time_passed += delta
+	if time_passed >= 1:
+		write_to_output("A second has passed!")
+		time_passed -= 1
+
+func _on_auto_play_action(resource : AutoPlaySuiteActionResource):
+	if resource.float_var == 0:
+		write_to_output("It was like this!")
+	else:
+		write_to_output("It was like that!")
+	
