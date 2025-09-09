c2fe0435bb2326b451e0d21fbbd911111baae0ae
Teddy Sjöström <teddy.tyler@gmail.com>
2025-09-09 11:05:17 +0200

    Logging: added a AutoPlaySuiteLogger base class that's meant to be extended by users

---
+++ addons/auto_play_suite_editor/logging/auto_play_suite_logger.gd
@@ -0,0 +1,41 @@
+extends Node
+class_name AutoPlaySuiteLogger
+
+var forward_output_to_editor : bool = false
+
+var dictionary_log : Dictionary[String, Variant]
+var output_log : Array[String]
+
+func setup():
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_auto_play_action(resource):
+	pass
+	
+func write_to_output(string):
+	output_log.append(string)
+	if forward_output_to_editor:
+		print(string)
+
+func write_as_entry(key : String, data):
+	dictionary_log[key] = data
+
+func log_to_list_entry(key : String, data, also_to_output : bool):
+	var array : Array = dictionary_log.get_or_add(key, [])
+	array.append(data)
+	if also_to_output:
+		write_to_output(data)
+
+static func instantiate_by_class_name(c_name: String) -> Object:
+	for entry in ProjectSettings.get_global_class_list():
+		if entry["class"] == c_name:
+			var script: Script = load(entry["path"])
+			var instance = script.new()
+			if instance is AutoPlaySuiteLogger:
+				return instance
+			else:
+				printerr("Tried instancing the class ", c_name, " but it was not present in the global class list!")
+	return null
