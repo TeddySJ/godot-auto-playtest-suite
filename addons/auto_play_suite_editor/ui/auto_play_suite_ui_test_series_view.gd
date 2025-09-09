0dc0eb25b4ea1a7a312cccc0ded211467b9ca8d9
Teddy Sjöström <teddytyler@gmail.com>
2025-09-09 17:08:34 +0200

    TestSeriesView: There is now a Series Name field at the top where that value can be changed

--- addons/auto_play_suite_editor/ui/auto_play_suite_ui_test_series_view.gd
+++ addons/auto_play_suite_editor/ui/auto_play_suite_ui_test_series_view.gd
@@ -1,19 +1,40 @@
 extends Control
 class_name AutoPlaySuiteUiTestSeriesView
 
+var test_series_name_input : LineEdit
+
 var base_panel_container : PanelContainer
 var hbox_container : HBoxContainer
 var test_button_list : Array[Button]
 
 var underlying_dictionary : Dictionary[Button, AutoPlaySuiteTestResource]
 
+var current_test_series : AutoPlaySuiteTestSeriesResource
+
 func _ready() -> void:
+	if current_test_series == null:
+		current_test_series = AutoPlaySuiteTestSeriesResource.new()
+	
 	var ed_scale : float = 1
 	if Engine.is_editor_hint():
 		ed_scale = EditorInterface.get_editor_scale()
 	
+	test_series_name_input = LineEdit.new()
+	test_series_name_input.position = Vector2(20, 0) * ed_scale
+	test_series_name_input.custom_minimum_size.x = 300 * ed_scale
+	test_series_name_input.text_changed.connect(_test_series_name_changed)
+	add_child(test_series_name_input)
+	
+	var test_series_name_label = Label.new()
+	test_series_name_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+	test_series_name_label.text = "Series Name:"
+	test_series_name_label.position = test_series_name_input.position + Vector2(-210, 5)  * ed_scale
+	test_series_name_label.custom_minimum_size.x = 200 * ed_scale
+	add_child(test_series_name_label)
+
+	
 	base_panel_container = PanelContainer.new()
-	base_panel_container.position = Vector2(0, 30) * ed_scale
+	base_panel_container.position = Vector2(0, 35) * ed_scale
 	base_panel_container.custom_minimum_size = Vector2(custom_minimum_size.x * 0.6, 50)
 	add_child(base_panel_container)
 	var scroll_container := ScrollContainer.new()
@@ -39,13 +60,21 @@ func _ready() -> void:
 	add_button(resource)
 	add_button(resource)
 
+func _test_series_name_changed(new_text : String):
+	current_test_series.test_series_name = new_text
+
 func add_button(test_resource : AutoPlaySuiteTestResource):
 	var button := Button.new()
 	button.text = test_resource.test_name
 	test_button_list.append(button)
 	underlying_dictionary[button] = test_resource
+	button.pressed.connect(func():_test_button_pressed(button))
 	button.focus_mode = Control.FOCUS_NONE
 	hbox_container.add_child(button)
+
+func _test_button_pressed(button_pressed : Button):
+	print("Test name: ", button_pressed.text, " Series name: ", current_test_series.test_series_name)
+	pass
 
 func clear():
 	for button in test_button_list:
