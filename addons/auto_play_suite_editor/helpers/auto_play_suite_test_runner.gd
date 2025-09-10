8fc94c4c1e7f739c01b34db30ae518e8abd1d9c1
Teddy Sjöström <teddytyler@gmail.com>
2025-09-10 16:50:44 +0200

    Refactor: Moved the responsibility of initializing tests to the TestRunner

--- addons/auto_play_suite_editor/helpers/auto_play_suite_test_runner.gd
+++ addons/auto_play_suite_editor/helpers/auto_play_suite_test_runner.gd
@@ -71,6 +71,24 @@ func _testing_finished():
 	current_action = null
 	actions_to_do = []
 
+static func start_testing():
+	var path := OS.get_environment("AutoTestPath")
+	var test : AutoPlaySuiteTestResource = load(path)
+	_run_test.call_deferred(test)
+	var logger : AutoPlaySuiteLogger = AutoPlaySuiteLogger.instantiate_by_class_name("AutoPlaySuiteDefaultLogger")
+	Engine.get_main_loop().root.add_child.call_deferred(logger)
+
+static func _run_test(test : AutoPlaySuiteTestResource):
+	print("Starting Test")
+	
+	# TODO: Centralize location pointing to all scripts with custom definitions
+	var custom_instructions = load("res://addons/auto_play_suite_editor/custom_instructions/custom_auto_play_instructions.gd").new()
+	custom_instructions.hook_into_suite()
+	
+	var test_runner := AutoPlaySuiteTestRunner.instance()
+	test_runner._start_test(test)
+
+
 static func instance() -> AutoPlaySuiteTestRunner:
 	if Singleton != null:
 		return Singleton
--- game.gd
+++ game.gd
@@ -3,19 +3,4 @@ class_name Game
 
 func _ready() -> void:
 	if OS.get_environment("DoAutoTesting") == "true":
-		var path := OS.get_environment("AutoTestPath")
-		var test : AutoPlaySuiteTestResource = load(path)
-		_run_test.call_deferred(test)
-		var logger : AutoPlaySuiteLogger = AutoPlaySuiteLogger.instantiate_by_class_name("AutoPlaySuiteDefaultLogger")
-		Engine.get_main_loop().root.add_child.call_deferred(logger)
-	
-
-func _run_test(test : AutoPlaySuiteTestResource):
-	print("Starting Test")
-	
-	# TODO: Centralize location pointing to all scripts with custom definitions
-	var custom_instructions = load("res://addons/auto_play_suite_editor/custom_instructions/custom_auto_play_instructions.gd").new()
-	custom_instructions.hook_into_suite()
-	
-	var test_runner := AutoPlaySuiteTestRunner.instance()
-	test_runner._start_test(test)
+		AutoPlaySuiteTestRunner.start_testing()
