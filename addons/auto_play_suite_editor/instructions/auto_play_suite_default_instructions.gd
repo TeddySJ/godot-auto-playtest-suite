9dffbb5a1288976e093d39ed050b031360160ca5
Teddy Sjöström <teddytyler@gmail.com>
2025-09-12 21:39:00 +0200

    Renamed "custom_instructions" folder to "instructions"

--- addons/auto_play_suite_editor/instructions/auto_play_suite_default_instructions.gd
+++ addons/auto_play_suite_editor/instructions/auto_play_suite_default_instructions.gd
@@ -8,6 +8,8 @@ var instruction_dictionary : Dictionary[StringName, AutoPlaySuiteInstructionDefi
 	&"[Wait] Wait X Seconds" : AutoPlaySuiteInstructionDefinition.Create(_wait_x_seconds, "Waits for [float] seconds"),
 	&"[Logging] Start Logger" : AutoPlaySuiteInstructionDefinition.Create(_start_logger, "Instances a logger of [string] class"),
 	&"[Logging] Instruct Logger" : AutoPlaySuiteInstructionDefinition.Create(_instruct_logger, "Instructs an existing logger of [string] class"),
+	&"[Eval] Start Evaluator" : AutoPlaySuiteInstructionDefinition.Create(_start_evaluator, "Instances an evaluator of [string] class"),
+	&"[Eval] Make Evaluation" : AutoPlaySuiteInstructionDefinition.Create(_instruct_evaluator, "Instructs an existing evaluator. Syntax: '[class_name]:[instruction]'"),
 }
 
 func hook_into_suite():
@@ -53,6 +55,26 @@ func _instruct_logger(arguments : AutoPlaySuiteActionResource):
 		return
 	
 	logger._on_instruction(arguments)
+
+func _start_evaluator(arguments : AutoPlaySuiteActionResource):
+	arguments.finished = true
+	var logger : AutoPlaySuiteLogger = AutoPlaySuiteLogger.instantiate_by_class_name(arguments.string_var)
+	if logger == null:
+		print("Failed to create logger of type: ", arguments.string_var)
+		return
+	
+	Engine.get_main_loop().root.add_child.call_deferred(logger)
+
+func _instruct_evaluator(arguments : AutoPlaySuiteActionResource):
+	arguments.finished = true
+	var strings : Array[String] = arguments.string_var.split(":")
+	var evaluator : AutoPlaySuiteEvaluator = AutoPlaySuiteEvaluator.get_evaluator_by_class_name(strings[0])
+	if evaluator == null:
+		print("Failed to get evaluator of type: ", strings[0])
+		return
+	
+	evaluator._on_instruction(arguments)
+
 
 func _wait_x_seconds(arguments : AutoPlaySuiteActionResource):
 	await Engine.get_main_loop().create_timer(arguments.float_var).timeout
